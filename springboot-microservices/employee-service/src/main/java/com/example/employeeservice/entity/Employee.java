e6ffcff5aeb1190822c6cd3959dd28a34d08dba6
unknown <sunidhijohri08@gmail.com>
2025-07-20 16:16:25 +0530

    done

---
+++ springboot-microservices/employee-service/src/main/java/com/example/employeeservice/entity/Employee.java
@@ -0,0 +1,26 @@
+package com.example.employeeservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(nullable = false,unique = true)
+   private String email;
+    private String departmentCode;
+}
